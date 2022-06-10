c523ed766510a06c190ac627dc7da9c0dfb4c03b
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-10 18:33:29 -0400

    Update npc.java

--- Sprint 1 (V1.1)/npc.java
+++ Sprint 1 (V1.1)/npc.java
@@ -1,1 +1,16 @@
 
+
+
+public class npc {
+   
+   private String name;
+   private int health;
+   public String location;
+  
+    public Character (String name, int health, String location){
+      this.name = name;
+      this.health = 20;
+      this.location = location;
+   }
+  
+}
