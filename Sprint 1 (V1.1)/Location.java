990f266a7ae7e01f234d9a77f1d1dc21402678ec
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-11 21:24:55 -0400

    Create Location.java

---
+++ Sprint 1 (V1.1)/Location.java
@@ -0,0 +1,14 @@
+
+public class Location {
+  
+  private int difficulty;
+  private String locationName;
+  private String type;
+  private int pointGoodRep;
+  private int pointBadRep;
+  
+  
+   // constants
+   private static String SETTLEMENT_TYPE = Settlement;
+   private static int MAX_REPUTATION = 20;
+}
