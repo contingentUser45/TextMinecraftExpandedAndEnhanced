0b46633851ae793caa30f5e7d73938d07c818034
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-14 09:57:28 -0400

    fixed up

--- Sprint 1 (V1.1)/LocationInfo.java
+++ Sprint 1 (V1.1)/LocationInfo.java
@@ -1,7 +1,6 @@
 
 public class Location {
   
-  private int difficulty;
   private String locationName;
   private int pointGoodRep;
   private int x1;
@@ -11,13 +10,11 @@ public class Location {
   
   
    // constants
-   private static String SETTLEMENT_TYPE = Settlement;
    public static int MAX_REPUTATION = 100;
-   public static int DEFAULT_REP = 0;
+   public static int DEFAULT_REP = 50;
   
    public Location (int difficulty, locationName, String type, int pointGoodRep,
     int x1, int y1, int x2 int y2){
-      this.difficulty = difficulty;
       this.locationName = locationName;
       this.pointGoodRep = DEFAULT_REP;
       this.pointBadRep = DEFAULT_REP;
@@ -32,17 +29,33 @@ public class Location {
    }
    
    public int getHealth() {
-      if (pointGoodRep < 0){
-         return "hated";
+      if (pointGoodRep <= 0){
+         return "Hated";
       }
-      else if (pointGoodRep < 0)
+         else if ((pointGoodRep > 0) && (pointGoodRep <= 50)){
+         return "Suspicious";
+      }
+          else if ((pointGoodRep > 50) && (pointGoodRep <= 75)){
+         return "Welcomed";
+      }
+            else if ((pointGoodRep > 75) && (pointGoodRep <= 100)){
+         return "Well Liked";
+      }
+      else {
+         return "Idolized";
+      }
    }
    
-   public int getX() {
-      return this.x;
+   public String getlocationName() {
+      return this.getlocationName;
    }
    
-   public int getY() {
-      return this.y;
+   public int getReputation() {
+      return this.pointGoodRep;
+   }
+
+   // mutators 
+   public void setName(String newName) {
+      this.name = newName;
    }
 }
