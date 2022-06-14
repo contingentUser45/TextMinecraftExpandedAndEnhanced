fe9f4596ca03f9ad27b6c50bf8855005ea0674a4
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-14 14:59:22 -0400

    Update Location.java

--- Sprint 1 (V1.1)/Location.java
+++ Sprint 1 (V1.1)/Location.java
@@ -2,7 +2,7 @@
 public class Location {
   
   private String locationName;
-   private String reputation;
+  private String reputation;
   private int pointGoodRep;
   private int x1;
   private int y1;
@@ -18,7 +18,6 @@ public class Location {
     int x1, int y1, int x2 int y2){
       this.locationName = locationName;
       this.pointGoodRep = DEFAULT_REP;
-      this.pointBadRep = DEFAULT_REP;
       this.x1 = x1;
       this.y1 = y1;
       this.x2 = x2;
