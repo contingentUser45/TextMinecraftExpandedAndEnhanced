438536fd17c4fe5adae00f817c9fe8422ee25ba1
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-14 09:30:04 -0400

    Update and rename Location.java to LocationInfo.java

---
+++ Sprint 1 (V1.1)/LocationInfo.java
@@ -0,0 +1,29 @@
+
+public class Location {
+  
+  private int difficulty;
+  private String locationName;
+  private String type;
+  private int pointGoodRep;
+  private int pointBadRep;
+  public int x1;
+  public int y1;
+  public int x2;
+  public int y2;
+  
+  
+   // constants
+   private static String SETTLEMENT_TYPE = Settlement;
+   public static int MAX_REPUTATION = 100;
+  
+   public Location (int difficulty, locationName, String type, int pointGoodRep, 
+                     int pointBadRep,int x1, int y1){
+      this.difficulty = name;
+      this.health = MAX_HEALTH;
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+      this.dead = dead;
+   }
+}
