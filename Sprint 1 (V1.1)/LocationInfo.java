99a49b1cb198d5569832cd0790ee3645356185fd
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-14 09:37:44 -0400

    Update LocationInfo.java

--- Sprint 1 (V1.1)/LocationInfo.java
+++ Sprint 1 (V1.1)/LocationInfo.java
@@ -3,7 +3,6 @@ public class Location {
   
   private int difficulty;
   private String locationName;
-  private String type;
   private int pointGoodRep;
   private int pointBadRep;
   public int x1;
@@ -15,15 +14,17 @@ public class Location {
    // constants
    private static String SETTLEMENT_TYPE = Settlement;
    public static int MAX_REPUTATION = 100;
+   public static int DEFAULT_REP = 0;
   
    public Location (int difficulty, locationName, String type, int pointGoodRep, 
-                     int pointBadRep,int x1, int y1){
-      this.difficulty = name;
-      this.health = MAX_HEALTH;
+                     int pointBadRep, nt x1, int y1, int x2 int y2){
+      this.difficulty = difficulty;
+      this.locationName = locationName;
+      this.pointGoodRep = DEFAULT_REP;
+      this.pointBadRep = DEFAULT_REP;
       this.x1 = x1;
       this.y1 = y1;
       this.x2 = x2;
       this.y2 = y2;
-      this.dead = dead;
    }
 }
