33331e39cd16ac7be8ba821a8bb3469938efec48
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-06-14 09:07:59 -0400

    Update and rename npc.java to Npc.java

--- Sprint 1 (V1.1)/Npc.java
+++ Sprint 1 (V1.1)/Npc.java
@@ -1,13 +1,13 @@
 
 
 
-public class npc {
+public class Npc {
    
    private String name;
    private int health;
    public String location;
   
-    public Character (String name, int health, String location){
+    public Npc (String name, int health, String location){
       this.name = name;
       this.health = 20;
       this.location = location;
